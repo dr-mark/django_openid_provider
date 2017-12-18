81b27db3ce7944ccbd7a8fd503bbf4ea30152483
Mark Allen <mja@boron.mark-allen.net>
2017-12-18 14:49:01 +0000

    Update urls.py format for Django 1.11

--- openid_provider/urls.py
+++ openid_provider/urls.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4 sw=4 : */
 
-try:
-    from django.conf.urls import patterns, url
-except ImportError:  # Django < 1.4
-    from django.conf.urls.defaults import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('openid_provider.views',
-    url(r'^$', 'openid_server', name='openid-provider-root'),
-    url(r'^decide/$', 'openid_decide', name='openid-provider-decide'),
-    url(r'^xrds/$', 'openid_xrds', name='openid-provider-xrds'),
-    url(r'^(?P<id>.*)/$', 'openid_xrds', {'identity': True}, name='openid-provider-identity'),
+from openid_provider.views import (
+    openid_server, openid_decide, openid_xrds,
 )
+
+urlpatterns = [
+    url(r'^$', openid_server, name='openid-provider-root'),
+    url(r'^decide/$', openid_decide, name='openid-provider-decide'),
+    url(r'^xrds/$', openid_xrds, name='openid-provider-xrds'),
+    url(r'^(?P<id>.*)/$', openid_xrds, {'identity': True}, name='openid-provider-identity'),
+]
